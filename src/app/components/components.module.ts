965eff80028611b0a9fc5d5957681f6216e011d5
Jhon Castillo <jcastillo@dlya.com.uy>
2023-04-12 10:51:54 -0500

    add angular material in project

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports:[ToolbarComponent]
+})
+export class ComponentsModule { }
