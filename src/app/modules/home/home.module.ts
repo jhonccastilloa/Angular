d2ffd71d30fcd3e0f4c7b0009ead25b4d85edf30
Jhon Castillo <jcastillo@dlya.com.uy>
2023-04-12 01:49:26 -0500

    crear routing a home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,9 +1,10 @@
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
