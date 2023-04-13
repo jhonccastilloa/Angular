ce5e1f10f75dec3c8f680ac7962a8479efe3c187
Jhon Castillo <jcastillo@dlya.com.uy>
2023-04-13 17:13:55 -0500

    create tabledata

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,25 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatIconModule } from '@angular/material/icon';
+import { PeopleTableComponent } from './people-table/people-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [ToolbarComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule],
-  exports: [ToolbarComponent, MatToolbarModule, MatIconModule],
+  declarations: [ToolbarComponent, PeopleTableComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSortModule,
+  ],
+  exports: [ToolbarComponent, PeopleTableComponent],
 })
 export class ComponentsModule {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule,ComponentsModule],
 })
 export class HomeModule {}
