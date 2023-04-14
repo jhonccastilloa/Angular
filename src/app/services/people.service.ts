f0cb63be6fc670839addc5584bd43e50f2c28fe2
Jhon Castillo <jcastillo@dlya.com.uy>
2023-04-14 14:44:12 -0500

    save data in locale Storage

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -9,22 +9,42 @@ export class PeopleService {
   newPeople: Person[] = [];
   favoritePeople: Person[] = [];
   constructor() {
-    this.newPeople = people.map((person) => ({
-      isChecked: false,
-      ...person,
-    }));
+    this.initData();
   }
 
+  initData() {
+    const peopleStorage = localStorage.getItem('people');
+    const favoritePeopleStorage = localStorage.getItem('favoritePeople');
+
+    this.favoritePeople = favoritePeopleStorage
+      ? JSON.parse(favoritePeopleStorage)
+      : [];
+    this.newPeople = peopleStorage
+      ? JSON.parse(peopleStorage)
+      : people.map((person) => ({
+          isChecked: false,
+          ...person,
+        }));
+  }
   addFavorite(person: Person) {
     this.favoritePeople = person.isChecked
       ? [...this.favoritePeople, person]
       : this.favoritePeople.filter((per) => per.id !== person.id);
+    DOMException;
+    this.onLocalStorage();
   }
 
   removeFavorite(person: Person) {
     this.favoritePeople = this.favoritePeople.filter(
       (per) => per.id !== person.id
     );
+    this.onLocalStorage();
     return this.favoritePeople;
+  }
+
+  onLocalStorage() {
+    console.log(this.newPeople)
+    localStorage.setItem('favoritePeople', JSON.stringify(this.favoritePeople));
+    localStorage.setItem('people', JSON.stringify(this.newPeople));
   }
 }
